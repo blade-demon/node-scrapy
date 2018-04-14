62ead2cd87832ca210ce270a2055cd2ea99af809
ziwei <ziwei@gamepoch.com>
2018-04-14 16:49:56 +0800

    fix bugs

--- scripts/91porn.js
+++ scripts/91porn.js
@@ -1,4 +1,17 @@
 const cheerio = require('cheerio');
-const axios = require('axios');
+const request = require('request-promise');
 const fs = require('fs');
 const _ = require('lodash');
+
+let options = {
+    uri: "http://91porn.com/v.php?category=mf&viewtype=basic",
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
+
+request(options).then(function ($) {
+    console.log($.fn.jquery);
+}).catch(function (err) {
+    console.log("获取网页数据出错！");
+});
